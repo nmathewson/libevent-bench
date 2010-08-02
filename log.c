639b005439757fa9ea987155329b66850d508f86
Christopher Davis <chrisd@mangrin.org>
2010-08-02 01:46:41 -0700

    Add logging module.

---
+++ log.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "log.h"
+#include "util.h"
+
+static enum log_level min_log_level = LOG_NOTICE;
+static FILE *log_file = NULL;
+static int log_do_scrub = 1;
+
+void
+log_debug(const char *msg, ...)
+{
+	va_list ap;
+
+	va_start(ap, msg);
+	log_msg_va(LOG_DEBUG, 0, msg, ap);
+	va_end(ap);
+}
+
+void
+log_info(const char *msg, ...)
+{
+	va_list ap;
+
+	va_start(ap, msg);
+	log_msg_va(LOG_INFO, 0, msg, ap);
+	va_end(ap);
+}
+
+void
+log_notice(const char *msg, ...)
+{
+	va_list ap;
+
+	va_start(ap, msg);
+	log_msg_va(LOG_NOTICE, 0, msg, ap);
+	va_end(ap);
+}
+
+void
+log_warn(const char *msg, ...)
+{
+	va_list ap;
+
+	va_start(ap, msg);
+	log_msg_va(LOG_WARN, 0, msg, ap);
+	va_end(ap);
+}
+
+void
+log_error(const char *msg, ...)
+{
+	va_list ap;
+
+	va_start(ap, msg);
+	log_msg_va(LOG_ERROR, 0, msg, ap);
+	va_end(ap);
+}
+
+void
+log_socket_error(const char *msg, ...)
+{
+	va_list ap;
+
+	va_start(ap, msg);
+	log_msg_va(LOG_ERROR, 1, msg, ap);
+	va_end(ap);
+}
+
+void
+log_fatal(const char *msg, ...)
+{
+	va_list ap;
+
+	va_start(ap, msg);
+	log_msg_va(LOG_FATAL, 0, msg, ap);
+	va_end(ap);
+}
+
+void
+log_msg_va(enum log_level lvl, int serr, const char *msg, va_list ap)
+{
+	if (lvl >= min_log_level) {
+		vfprintf(log_file, msg, ap);
+		if (serr)
+			fprintf(log_file, ": %s", socket_error_string(-1));
+		fputs("\n", log_file);
+		fflush(log_file);
+		if (lvl >= LOG_FATAL)
+			abort();
+	}
+}
+
+void
+log_set_min_level(enum log_level lvl)
+{
+	min_log_level = lvl;
+}
+
+enum log_level
+log_get_min_level(void)
+{
+	return min_log_level;
+}
+
+void
+log_set_file(FILE *fp)
+{
+	if (!fp)
+		log_file = stderr;
+	else
+		log_file = fp;
+}
---
+++ log.h
@@ -0,0 +1,36 @@
+#ifndef _LOG_H_
+#define _LOG_H_
+
+#include <stdarg.h>
+#include <stdio.h>
+
+enum log_level {
+	LOG_DEBUG,
+	LOG_INFO,
+	LOG_NOTICE,
+	LOG_WARN,
+	LOG_ERROR,
+	LOG_FATAL
+};
+
+#ifdef __GNUC__
+#define CHECK_FMT(a,b) __attribute__((format(printf, a, b)))
+#else
+#define CHECK_FMT(a,b)
+#endif
+
+void log_debug(const char *msg, ...) CHECK_FMT(1,2);
+void log_info(const char *msg, ...) CHECK_FMT(1,2);
+void log_notice(const char *msg, ...) CHECK_FMT(1,2);
+void log_warn(const char *msg, ...) CHECK_FMT(1,2);
+void log_error(const char *msg, ...) CHECK_FMT(1,2);
+void log_socket_error(const char *msg, ...) CHECK_FMT(1,2);
+void log_fatal(const char *msg, ...) CHECK_FMT(1,2);
+
+void log_msg_va(enum log_level lvl, int serr, const char *msg, va_list ap);
+
+void log_set_min_level(enum log_level lvl);
+enum log_level log_get_min_level(void);
+void log_set_file(FILE *fp);
+
+#endif
